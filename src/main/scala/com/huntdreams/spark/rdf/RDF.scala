e0a8bc5952a04dbbeddf8ce59aea53ead136a71e
noprom <tyee.noprom@qq.com>
2016-07-26 17:58:09 +0800

    RDF

--- src/main/scala/com/huntdreams/spark/rdf/RDF.scala
+++ src/main/scala/com/huntdreams/spark/rdf/RDF.scala
@@ -9,19 +9,19 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
- * 决策树和随即森林
- *
- * @author tyee.noprom@qq.com
- * @time 2/16/16 10:08 AM.
- */
+  * 决策树和随即森林
+  *
+  * Author tyee.noprom@qq.com
+  * Time 2/16/16 10:08 AM.
+  */
 object RDF {
 
   def main(args: Array[String]) {
     val sc = new SparkContext(new SparkConf().setAppName("RDF"))
-    val rawData = sc.textFile("resources/covtype.data");
+    val rawData = sc.textFile("resources/covtype.data")
 
     // 准备数据
-    val data = rawData.map{ line =>
+    val data = rawData.map { line =>
       val values = line.split(",").map(_.toDouble)
       // .init 返回除了最后一列的元素,并将其转为vector
       val featureVector = Vectors.dense(values.init)
@@ -38,12 +38,21 @@ object RDF {
     testData.cache()
 
     // 开始训练并且分析数据
+    simpleDecisionTree(trainData, cvData)
+    randomClassifier(trainData, cvData)
   }
 
   def simpleDecisionTree(trainData: RDD[LabeledPoint], cvData: RDD[LabeledPoint]): Unit = {
     // 创建一个简单默认的决策树模型
     val model = DecisionTree.trainClassifier(trainData, 7, Map[Int, Int](), "gini", 4, 100)
+    val metrics = getMetrics(model, cvData)
 
+    println(metrics.confusionMatrix)
+    println(metrics.precision)
+
+    (0 until 7).map(
+      category => (metrics.precision(category), metrics.recall(category))
+    ).foreach(println)
   }
 
   def getMetrics(model: DecisionTreeModel, data: RDD[LabeledPoint]): MulticlassMetrics = {
@@ -51,5 +60,22 @@ object RDF {
       (model.predict(example.features), example.label)
     )
     new MulticlassMetrics(predictionsAndLabels)
+  }
+
+  def randomClassifier(trainData: RDD[LabeledPoint], cvData: RDD[LabeledPoint]): Unit = {
+    val trainPriorProbabilities = classProbabilities(trainData)
+    val cvPriorProbabilities = classProbabilities(cvData)
+    val accuracy = trainPriorProbabilities.zip(cvPriorProbabilities).map {
+      case (trainProb, cvProb) => trainProb * cvProb
+    }.sum
+    println(accuracy)
+  }
+
+  def classProbabilities(data: RDD[LabeledPoint]): Array[Double] = {
+    // count category, count in data
+    val countsByCategory = data.map(_.label).countByValue()
+    // order counts by category and extract counts
+    val counts = countsByCategory.toArray.sortBy(_._1).map(_._2)
+    counts.map(_.toDouble / counts.sum)
   }
 }
